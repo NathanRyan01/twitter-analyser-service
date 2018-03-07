d779598acda8566ffc2b8e51bc7edfd0cee824d8
Vishal Pandey <pandeyvishal1990@gmail.com>
2018-03-07 16:52:43 +0000

    twit installed - searchiing tweets

---
+++ app/config.js
@@ -0,0 +1,8 @@
+var keys = {
+    "consumer_key":	"8qqhd9JJM7EFbjYPceZKVAXdc",
+    "consumer_secret":	"Rkun9Br8OwVu3L7K3EW3yBgbeZPMgqBcYw8Q3kHAFUcfAwEvkm",
+    "access_token": "1620728316-RGW55yyFnqAO7jBcpg5KDx1FaYj1y4pVheQNw7n",
+    "access_token_secret": "tXDnr0XLCxf7BDvnviVO4kNG4fx0oEaGHn8LcXtaixunE",
+    timeout_ms:           60*1000,
+}
+module.exports = keys
